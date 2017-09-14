ab133b2fb281c325e1b981a31bf185c522a85c91
zhaohui02 <zhaohui02@yxqiche.com>
2017-09-14 15:57:00 +0800

    添加了方法fangfa

--- app/src/main/java/com/example/zhaohui02/myapplication/WuYongActivity.java
+++ app/src/main/java/com/example/zhaohui02/myapplication/WuYongActivity.java
@@ -1,8 +1,14 @@
 package com.example.zhaohui02.myapplication;
 
+import android.util.Log;
+
 /**
  * Created by zhaohui02 on 2017/9/14.
  */
 
 public class WuYongActivity {
+
+   public void FangFa(){
+       Log.e("123","123");
+   }
 }
